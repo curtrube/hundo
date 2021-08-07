6a3bcad4733da63339514c510ddf57b9929882f5
Curtis Rubeck <rubeckcurtis@gmail.com>
2021-08-06 22:24:21 -0500

    added code to print results to table

--- js/site.js
+++ js/site.js
@@ -1,3 +1,25 @@
-const helloWorld = () => {
-  alert('Hello World');
+const getResults = () => {
+  let minValue = document.getElementById('minValueInput').value;
+  let maxValue = document.getElementById('maxValueInput').value;
+
+  if (minValue) {
+    const results = {
+      minValue: minValue,
+      maxValue: maxValue,
+    };
+    console.log(results);
+  }
+
+  let tableBody = document.getElementById('resultsBody');
+  tableBody.innerHTML = ``;
+
+  for (let i = minValue; i <= maxValue; i++) {
+    let tableRow = document.createElement('tr');
+    let tableData = document.createElement('td');
+    tableData.innerHTML = `${i}`;
+    tableRow.appendChild(tableData);
+    tableBody.appendChild(tableRow);
+  }
+  let minValue2 = document.getElementById('minValueInput');
+  minValue2.setAttribute('placeholder', 'Min');
 };
